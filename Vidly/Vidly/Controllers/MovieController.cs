0014a2d49bee6f6bac247e2649f3364e3798f956
Vincent Finn <vincent.s.finn@gmail.com>
2019-03-21 15:18:11 -0400

    routing concepts

--- Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -9,11 +9,39 @@ namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
+
         // GET: Movie
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month); 
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("MovieID: " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if(String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
     }
 }
