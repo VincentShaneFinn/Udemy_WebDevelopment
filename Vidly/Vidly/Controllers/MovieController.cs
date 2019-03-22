961bb5a66da90ef895294c2c4810ea8950add3c8
Vincent Finn <vincent.s.finn@gmail.com>
2019-03-22 09:48:38 -0400

    view model

--- Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly/Vidly/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,34 +15,47 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month); 
-        }
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month); 
+        //}
 
-        public ActionResult Edit(int id)
-        {
-            return Content("MovieID: " + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("MovieID: " + id);
+        //}
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if(!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if(String.IsNullOrEmpty(sortBy))
-            {
-                sortBy = "Name";
-            }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if(!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+        //    if(String.IsNullOrEmpty(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
     }
 }
