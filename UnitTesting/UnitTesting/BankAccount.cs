146026cdc9181b4e8b3abdca68cea31d713d9d1d
Vincent Finn <vincent.s.finn@gmail.com>
2019-03-28 18:43:01 -0400

    added testing files

---
+++ UnitTesting/UnitTesting/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    class BankAccount
+    {
+
+        public int Balance { get; private set; }
+        public BankAccount(int startingBalance)
+        {
+            Balance = startingBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+
+        }
+
+        public void withdraw(int amount)
+        {
+
+        }
+    }
+
+}
+
+[TestFixture]
+
+public class BankAccountTests
+{
+    [Test]
+    public void BankAccountShouldIncreaseOnPositiveDeposit()
+    {
+
+    }
+}
