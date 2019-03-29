14425973b11dabe00d8e6a22af05c343f2758e22
Vincent Finn <vincent.s.finn@gmail.com>
2019-03-28 20:54:08 -0400

    more testing examples

--- UnitTesting/UnitTesting/BankAccount.cs
+++ UnitTesting/UnitTesting/BankAccount.cs
@@ -16,24 +16,119 @@ namespace UnitTesting
 
         public void Deposit(int amount)
         {
-
+            if(amount <= 0)
+            {
+                throw new ArgumentException("Non Positive Number", nameof(amount));
+            }
+            Balance += amount;
         }
 
-        public void withdraw(int amount)
+        public bool withdraw(int amount)
         {
-
+            if(Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
         }
     }
 
-}
-
-[TestFixture]
-
-public class BankAccountTests
-{
-    [Test]
-    public void BankAccountShouldIncreaseOnPositiveDeposit()
+    [TestFixture]
+    public class BankAccountTests
     {
+        [Test]
+        public void SampleAsserts()
+        {
+            //Success
+            //Assert.That(2 + 2, Is.EqualTo(4));
+
+            //Failure
+            //Assert.That(2 + 2, Is.EqualTo(5));
+            //throw new Exception();
+            //Assert.Fail("This Test Will Fail");
+
+        }
+
+        [Test]
+        public void SampleWarnings()
+        {
+            //Inconclusive
+            //Assert.Inconclusive();
+
+            //Warning
+            //Assert.Warn("This is not good");
+
+            //Warn.If(2 + 2 != 5);
+            //Warn.If(2 + 2, Is.Not.EqualTo(5));
+            //Warn.If(() => 2 + 2, Is.Not.EqualTo(5).After(2000));
+
+            //Warn.Unless(2 + 2 == 5);
+            //Warn.Unless(2 + 2, Is.EqualTo(5));
+            //Warn.Unless(() => 2 + 2, Is.EqualTo(5).After(2000));
+        }
+
+        private BankAccount ba;
+
+        [SetUp]
+        public void Setup()
+        {
+            ba = new BankAccount(100);
+        }
+
+        [Test]
+        public void BankAccountShouldIncreaseOnPositiveDeposit()
+        {
+            //AAA
+
+            //Arange
+            //Initialized in Setup;
+
+            //Act
+            ba.Deposit(100);
+
+            //Assert
+            Assert.That(ba.Balance, Is.EqualTo(200));
+
+        }
+
+        [Test]
+        public void BankAccountShouldThrowOnNonPositiveDeposit()
+        {
+            var ex = Assert.Throws<ArgumentException>(
+                () => ba.Deposit(-1)
+            );
+
+            StringAssert.StartsWith("Non Positive Number", ex.Message);
+        }
+    }
+
+    [TestFixture]
+    public class DataDrivenTest
+    {
+
+        private BankAccount ba;
+
+        [SetUp]
+        public void Setup()
+        {
+            ba = new BankAccount(100);
+        }
+
+        [Test]
+        [TestCase(50,true,50)]
+        [TestCase(100,true,0)]
+        [TestCase(1000, false, 100)]
+        public void TestMultipleWithdrwalScenarios(int amountToWithdraw, bool shouldSucceed, int expectedBalance)
+        {
+            var result = ba.withdraw(amountToWithdraw);
+            //Warn.If(!result, "Failed for some reason");
+            Assert.Multiple(() => {
+                Assert.That(result, Is.EqualTo(shouldSucceed));
+                Assert.That(expectedBalance, Is.EqualTo(ba.Balance));
+            });
+            
+        }
 
     }
 }
---
+++ UnitTesting/UnitTesting/QuadraticTest.cs
@@ -0,0 +1,59 @@
+﻿using JetBrains.dotMemoryUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    public class QuadraticTest
+    {
+        public Tuple<double, double> Quadratic(double a, double b, double c)
+        {
+            var disc = b * b - 4 * a * c;
+            if (disc < 0)
+            {
+                throw new Exception("Cannot solve with complex roots");
+            }
+            else
+            {
+                var root = Math.Sqrt(disc);
+                return Tuple.Create(
+                    (-b + root) / 2 / a,
+                    (-b - root) / 2 / a);
+            }
+        }
+    }
+
+    [TestFixture]
+    public class EquationTest
+    {
+        [Test]
+        public void Test()
+        {
+            var result = new QuadraticTest().Quadratic(1, 10, 16);
+        }
+
+        [Test]
+        public void MemoryTest()
+        {
+            var checkpoint1 = dotMemory.Check();
+
+            // ... manipulations
+
+            var checkpoint2 = dotMemory.Check(memory =>
+            {
+                Assert.That(memory.GetTrafficFrom(checkpoint1)
+                    .Where(obj => obj.Interface.Is<IEnumerable<int>>())
+                    .AllocatedMemory.SizeInBytes, Is.LessThan(1000));
+            });
+
+            dotMemory.Check(memory =>
+            {
+                Assert.That(memory.GetObjects(
+                    where => where.Type.Is<QuadraticTest>()
+                    ).ObjectsCount, Is.EqualTo(0));
+            });
+        }
+    }
+}
